db777e40d6368c4c42d6fc6e575d220f6b878bdd
ROHIT KUMAR MEHER <meherrohit99@gmail.com>
2026-03-08 21:34:02 +0530

    fix slack notification

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,8 +1,10 @@
 def call(status) {
+
     slackSend(
         channel: "#jenkins-alerts",
         color: status == "SUCCESS" ? "good" : "danger",
         message: "${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-        webhookUrl: credentials('slack-webhook')
+        tokenCredentialId: "slack-webhook"
     )
+
 }
