02539ff0c7405badf5e9e6c3960912b70b5faa9f
ROHIT KUMAR MEHER <meherrohit99@gmail.com>
2026-03-08 21:26:32 +0530

    Change webhoow url

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,8 +1,8 @@
 def call(status) {
     slackSend(
-        channel: '#jenkins-alerts',
+        channel: "#jenkins-alerts",
         color: status == "SUCCESS" ? "good" : "danger",
         message: "${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-        tokenCredentialId: 'slack-webhook'
+        webhookUrl: credentials('slack-webhook')
     )
 }
