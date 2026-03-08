43f21b98147f3eda3c29a1ac0627611bd99b45fa
ROHIT KUMAR MEHER <meherrohit99@gmail.com>
2026-03-08 20:55:47 +0530

    updated the pipeline code

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,146 +1,60 @@
 def call(Map config) {
 
-pipeline {
+    pipeline {
+        agent any
 
-agent any
+        environment {
+            AWS_REGION = "us-east-1"
+        }
 
-environment {
+        stages {
 
-IMAGE_NAME = config.imageName
-ECR_REPO   = config.repo
-IMAGE_TAG  = "${BUILD_NUMBER}"
+            stage('Checkout Code') {
+                steps {
+                    checkout scm
+                }
+            }
 
-}
+            stage('SonarQube Scan') {
+                steps {
+                    sonarScan()
+                }
+            }
 
-stages {
+            stage('Filesystem Security Scan') {
+                steps {
+                    trivyScan()
+                }
+            }
 
-stage('Checkout') {
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        dockerBuild(config.imageName, config.imageTag)
+                    }
+                }
+            }
 
-steps {
-checkout scm
-}
+            stage('Push Docker Image') {
+                steps {
+                    script {
+                        pushECR(config.repo, config.imageName, config.imageTag)
+                    }
+                }
+            }
 
-}
+        }
 
-stage('Detect Project Type') {
+        post {
 
-steps {
+            success {
+                slackNotify("SUCCESS")
+            }
 
-script {
+            failure {
+                slackNotify("FAILED")
+            }
 
-PROJECT_TYPE = projectDetect()
-
-echo "Detected project type: ${PROJECT_TYPE}"
-
-}
-
-}
-
-}
-
-stage('SonarQube Scan') {
-
-steps {
-sonarScan(config.projectKey)
-}
-
-}
-
-stage('Quality Gate') {
-
-steps {
-
-timeout(time: 5, unit: 'MINUTES') {
-
-waitForQualityGate abortPipeline: true
-
-}
-
-}
-
-}
-
-stage('Trivy Scan') {
-
-steps {
-trivyScan()
-}
-
-}
-
-stage('Build') {
-
-steps {
-
-script {
-
-if(PROJECT_TYPE == "java") {
-
-sh "mvn clean package"
-
-}
-
-else if(PROJECT_TYPE == "node") {
-
-sh "npm install"
-
-}
-
-else if(PROJECT_TYPE == "python") {
-
-sh "pip install -r requirements.txt"
-
-}
-
-else if(PROJECT_TYPE == "docker") {
-
-dockerBuild(IMAGE_NAME, IMAGE_TAG)
-
-}
-
-}
-
-}
-
-}
-
-stage('Push Image') {
-
-steps {
-pushECR(ECR_REPO, IMAGE_NAME, IMAGE_TAG)
-}
-
-}
-
-}
-
-post {
-
-success {
-
-slackNotify("SUCCESS")
-
-emailext(
-subject: "SUCCESS: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-body: "Build Success\n\n${env.BUILD_URL}",
-to: "meherrohit99@gmail.com"
-)
-
-}
-
-failure {
-
-slackNotify("FAILED")
-
-emailext(
-subject: "FAILED: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-body: "Build Failed\n\n${env.BUILD_URL}",
-to: "meherrohit99@gmail.com"
-)
-
-}
-
-}
-
-}
+        }
+    }
 }
